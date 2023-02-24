1bf4163ba48c47f6bb08387d9b815da0cbb8a928
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-24 20:30:40 +0100

    24.02

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, 'Hello', null];
+// Tuple
+let array = [10, 'Emily', false];
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// Basic Types
+let id: number = 5;
+let company: string = 'Traversy Media';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, 'Hello', null];
+
+// Tuple
+let array: [number, string, boolean] = [10, 'Emily', false]
