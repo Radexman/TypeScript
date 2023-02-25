5fd74ccc5b9de8e7b9f2d77df97397c22d904c86
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-25 11:58:14 +0100

    25.02

--- dist/index.js
+++ dist/index.js
@@ -8,3 +8,22 @@ let ids = [1, 2, 3, 4, 5];
 let arr = [1, 'Hello', null];
 // Tuple
 let array = [10, 'Emily', false];
+class Student {
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+function greeter(person) {
+    return `Hello, my name is ${person.firstName} ${person.lastName} and I'm ${person.age}. My hobbies are ${person.hobbies[0]} ${person.hobbies[1]} and ${person.hobbies[2]}`;
+}
+const user = {
+    firstName: 'Radek',
+    lastName: 'Siek',
+    age: 23,
+    isMarried: false,
+    hobbies: ['fishig', 'hiking', 'guitar'],
+};
+document.body.textContent = greeter(user);
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,38 @@ let ids: number[] = [1, 2, 3, 4, 5];
 let arr: any[] = [1, 'Hello', null];
 
 // Tuple
-let array: [number, string, boolean] = [10, 'Emily', false]
+let array: [number, string, boolean] = [10, 'Emily', false];
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    age: number,
+    isMarried: boolean,
+    hobbies: string[],
+}
+
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+ 
+
+function greeter(person: Person) {
+    return `Hello, my name is ${person.firstName} ${person.lastName} and I'm ${person.age}. My hobbies are ${person.hobbies[0]} ${person.hobbies[1]} and ${person.hobbies[2]}`;
+} 
+
+const user: Person = {
+    firstName: 'Radek',
+    lastName: 'Siek',
+    age: 23,
+    isMarried: false,
+    hobbies: ['fishig', 'hiking', 'guitar'],
+}
+
+document.body.textContent = greeter(user);
