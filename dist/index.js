3972e3fb56e8c22255e0634e0aa3526861e4d160
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-03 12:29:45 +0100

    03.03

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,58 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Basic Types
-let id = 5;
-let company = 'Traversy Media';
-let isPublished = true;
+const id = 5;
+const company = 'Traversy Media';
+const isPublished = true;
+let x = 'Hello';
+x = true;
+let age;
+age = 23;
+let ids = [1, 2, 3];
+let arr = ['Hello', 1, false];
+// Tuple
+let person = [1, 'Emilia', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Emilia'],
+    [2, 'Radek'],
+    [3, 'Dawid'],
+];
+// Union
+let pid = '1';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+// Type Assertion
+let cid = 1;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const personOne = {
+    id: 1,
+    name: 'Emilia'
+};
+const p1 = 1;
+const add = (x, y) => x + y;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,88 @@
 // Basic Types
-let id: number = 5;
-let company: string = 'Traversy Media';
-let isPublished: boolean = true;
+const id: number = 5;
+const company: string = 'Traversy Media';
+const isPublished: boolean = true;
+let x: any = 'Hello';
+x = true;
+let age: number;
+age = 23;
+let ids: number[] = [1, 2, 3];
+let arr: any[] = ['Hello', 1, false];
 
+// Tuple
+let person: [number, string, boolean] = [1, 'Emilia', true];
 
-export{};
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+    [1, 'Emilia'],
+    [2, 'Radek'],
+    [3, 'Dawid'],
+]
+
+// Union
+let pid: number | string = '1';
+
+// Enum
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right,
+}
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right',
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string,
+}
+
+const user: User = {
+    id: 1,
+    name: 'John',
+}
+
+// Type Assertion
+let cid: any = 1;
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+    return x + y;
+}
+
+// Void
+function log(message: string | number): void {
+    console.log(message);
+}
+
+// Interfaces
+interface UserInterface {
+    readonly id: number,
+    name: string,
+    age?: number
+}
+
+const personOne: UserInterface = {
+    id: 1,
+    name: 'Emilia'
+}
+
+// Types
+type Point  = number | string;
+const p1: Point = 1;
+
+// Interfaces with function
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
